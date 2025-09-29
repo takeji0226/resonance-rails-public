dd763c3233496e626085b9d2706cd3a522079813
takeji <takeji0226@gmail.com>
2025-09-30 04:54:10 +0900

    feat: implement me in Rails

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,15 +6,21 @@ class Users::SessionsController < Devise::SessionsController
   skip_forgery_protection
 
   # 明示的に public な destroy を作って Rails 7.1 の厳格チェックを満たす
-  def destroy
-    super
+  def respond_to_on_destroy
+    head :no_content
   end
 
   private
 
   # 初回ユーザ確認の上、初回ユーザに対してオンボード画面を表示
   def respond_with(resource, _opts = {})
-    if resource.first_login_at.nil?
+  token = request.env['warden-jwt_auth.token']
+  response.set_header('Authorization', "Bearer #{token}") if token.present?
+
+  Rails.logger.info "===> respond_with called for #{resource.email}"
+  Rails.logger.info "===> respond_with called for #{response.header}"
+
+  if resource.first_login_at.nil?
       resource.update!(first_login_at: Time.current, onboarding_stage: "philosophy")
     end
     render json: { message: "signed_in", user: { id: resource.id, email: resource.email } }, status: :ok
